39a58b3b5b61e02aee479b207c56c75032590882
Anthony Brown <anthonybrown0528@protonmail.com>
2022-02-23 20:54:31 -0500

    fix issue related to scale of screen-space coordinates

--- pointcloud_proc_cpp/src/gen_pointcloud.cpp
+++ pointcloud_proc_cpp/src/gen_pointcloud.cpp
@@ -2,8 +2,10 @@
 
 using namespace std::chrono_literals;
 
-GenPointCloudNode::GenPointCloudNode(float hfov, float vfov, float near, float far) : Node("gen_pointcloud"), AXIS_COUNT(3) {
+GenPointCloudNode::GenPointCloudNode(float hfov, float vfov, float near, float far) : Node("gen_pointcloud"), AXIS_COUNT(3), SCALE_FACTOR(1.0f) {
   subscriber = this->create_subscription<sensor_msgs::msg::Image>("image", 2, std::bind(&GenPointCloudNode::subscriber_callback, this, std::placeholders::_1));
+  subCameraState = this->create_subscription<pointcloud_interfaces::msg::CameraState>("camera_state", 2, std::bind(&GenPointCloudNode::subscriber_camera_state_callback, this, std::placeholders::_1));
+
   publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("pointcloud", 2);
 
   //Clipping planes of simulated camera
@@ -33,12 +35,26 @@ GenPointCloudNode::GenPointCloudNode(float hfov, float vfov, float near, float f
   pointcloudMsg.point_step = (pointcloudMsg.fields.size() + 1) * sizeof(float);
 }
 
+void GenPointCloudNode::subscriber_camera_state_callback(const pointcloud_interfaces::msg::CameraState::SharedPtr msg) {
+  updateCameraState(msg);
+}
+
 void GenPointCloudNode::subscriber_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
   updateFromMsg(msg);
 
   flatData = std::vector<PointXYZ>(imageWidth * imageHeight);
 
   const cv_bridge::CvImagePtr image_ptr = cv_bridge::toCvCopy(msg);
+
+  if(abs(this->SCALE_FACTOR - 1.0f) < 0.001f) {
+    cv::Size origSize = image_ptr->image.size();
+    origSize.width *= this->SCALE_FACTOR;
+    origSize.height *= this->SCALE_FACTOR;
+
+    cv::resize(image_ptr->image, image_ptr->image, origSize, cv::INTER_CUBIC);
+  }
+
+
   image_ptr->image.convertTo(image_ptr->image, CV_32F, 1.0f / 255);
 
   gen_pointcloud(image_ptr);
@@ -53,9 +69,13 @@ void GenPointCloudNode::subscriber_callback(const sensor_msgs::msg::Image::Share
   publisher->publish(pointcloudMsg);
 }
 
+void GenPointCloudNode::updateCameraState(const pointcloud_interfaces::msg::CameraState::SharedPtr msg) {
+  this->camState = *msg;
+}
+
 void GenPointCloudNode::updateFromMsg(const sensor_msgs::msg::Image::SharedPtr msg) {
-  this->imageWidth = msg->width;
-  this->imageHeight = msg->height;
+  this->imageWidth = msg->width * this->SCALE_FACTOR;
+  this->imageHeight = msg->height * this->SCALE_FACTOR;
 
   pointcloudMsg.height = imageHeight;
   pointcloudMsg.width = imageWidth;
@@ -70,13 +90,14 @@ void GenPointCloudNode::updateFromMsg(const sensor_msgs::msg::Image::SharedPtr m
 
     for(unsigned int i = 0; i < this->u.size(); i++) {
     this->u[i] = i / (float)(this->imageWidth - 1);
-    this->u[i] = 0.5 - u[i];
-    this->u[i] *= -1;
+    this->u[i] = u[i] - 0.5f;
+    this->u[i] *= 2.0f;
     }
 
     for(unsigned int i = 0; i < this->v.size(); i++) {
       this->v[i] = i / (float)(this->imageHeight - 1);
-      this->v[i] = 0.5 - v[i];
+      this->v[i] = 0.5f - v[i];
+      this->v[i] *= 2.0f;
     }
   }
 }
@@ -97,6 +118,44 @@ void GenPointCloudNode::gen_pointcloud(const cv_bridge::CvImagePtr image_ptr) {
       this->flatData[i * this->imageWidth + j].y = image_ptr->image.at<float>(i, j) * this->u[j] * tan(this->hFov / 2.0f);
       this->flatData[i * this->imageWidth + j].z = image_ptr->image.at<float>(i, j) * this->v[i] * tan(this->vFov / 2.0f);
       this->flatData[i * this->imageWidth + j].x = image_ptr->image.at<float>(i, j);
+
+      //this->flatData[i * this->imageWidth + j] = this->flatData[i * this->imageWidth + j] - this->camState.translation;
+
+      std::array<float, 4> conjugateRotation;
+
+      conjugateRotation[0] = -this->camState.rotation[0];
+      conjugateRotation[1] = -this->camState.rotation[1];
+      conjugateRotation[2] = -this->camState.rotation[2];
+      conjugateRotation[3] =  this->camState.rotation[3];
+
+      rotate_point(conjugateRotation, this->flatData[i * this->imageWidth + j], this->camState.rotation);
     }
   }
+}
+
+std::array<float, 4> GenPointCloudNode::calc_hamilton_product(std::array<float, 4> a, std::array<float, 4> b) {
+  std::array<float, 4> newQuaternion;
+
+  newQuaternion[0] = a[3] * b[0] + a[0] * b[3] + a[1] * b[2] - a[2] * b[1];
+  newQuaternion[1] = a[3] * b[1] - a[0] * b[2] + a[1] * b[3] + a[2] * b[0];
+  newQuaternion[2] = a[3] * b[2] + a[0] * b[1] - a[1] * b[0] + a[2] * b[3];
+  newQuaternion[3] = a[3] * b[3] - a[0] * b[0] - a[1] * b[1] + a[2] * b[2];
+
+  return newQuaternion;
+}
+
+void GenPointCloudNode::rotate_point(std::array<float, 4> quaternion, PointXYZ &point, std::array<float, 4> conjugateQuaternion) {
+  std::array<float, 4> pointQuaternion;
+
+  pointQuaternion[0] = point.x;
+  pointQuaternion[1] = point.y;
+  pointQuaternion[2] = point.z;
+  pointQuaternion[3] = 0.0f;
+
+  std::array<float, 4> partialRotationCalculation = calc_hamilton_product(quaternion, pointQuaternion);
+  std::array<float, 4> fullRotationCalculation = calc_hamilton_product(partialRotationCalculation, conjugateQuaternion);
+
+  point.x = fullRotationCalculation[0];
+  point.y = fullRotationCalculation[1];
+  point.z = fullRotationCalculation[2];
 }
--- pointcloud_proc_cpp/src/gen_pointcloud.hpp
+++ pointcloud_proc_cpp/src/gen_pointcloud.hpp
@@ -10,6 +10,7 @@
 #include "sensor_msgs/msg/image.hpp"
 
 #include "pointcloud_interfaces/srv/camera_params.hpp"
+#include "pointcloud_interfaces/msg/camera_state.hpp"
 
 struct PointXYZ {
     float x;
@@ -17,6 +18,46 @@ struct PointXYZ {
     float z;
 
     float padding;
+
+    PointXYZ operator+(PointXYZ b) {
+      PointXYZ sum;
+
+      sum.x = this->x + b.x;
+      sum.y = this->y + b.y;
+      sum.z = this->z + b.z;
+
+      return sum;
+    }
+
+    PointXYZ operator+(std::array<float, 3> b) {
+      PointXYZ sum;
+
+      sum.x = this->x + b[0];
+      sum.y = this->y + b[1];
+      sum.z = this->z + b[2];
+
+      return sum;
+    }
+
+    PointXYZ operator-(PointXYZ b) {
+      PointXYZ difference;
+
+      difference.x = this->x - b.x;
+      difference.y = this->y - b.y;
+      difference.z = this->z - b.z;
+
+      return difference;
+    }
+
+    PointXYZ operator-(std::array<float, 3> b) {
+      PointXYZ difference;
+
+      difference.x = this->x - b[0];
+      difference.y = this->y - b[1];
+      difference.z = this->z - b[2];
+
+      return difference;
+    }
 };
 
 class GenPointCloudNode : public rclcpp::Node {
@@ -24,14 +65,22 @@ class GenPointCloudNode : public rclcpp::Node {
     GenPointCloudNode(float hfov, float vfov, float near, float far);
   private:
     void subscriber_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+    void subscriber_camera_state_callback(const pointcloud_interfaces::msg::CameraState::SharedPtr msg);
+
     void gen_pointcloud(const cv_bridge::CvImagePtr image_ptr);
+    void updateCameraState(const pointcloud_interfaces::msg::CameraState::SharedPtr msg);
     void updateFromMsg(const sensor_msgs::msg::Image::SharedPtr msg);
     void remove_boundary_values();
+    std::array<float, 4> calc_hamilton_product(std::array<float, 4> a, std::array<float, 4> b);
+    void rotate_point(std::array<float, 4> quaternion, PointXYZ &point, std::array<float, 4> inverseQuaternion);
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber;
+    rclcpp::Subscription<pointcloud_interfaces::msg::CameraState>::SharedPtr subCameraState;
+
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher;
 
     sensor_msgs::msg::PointCloud2 pointcloudMsg;
+    pointcloud_interfaces::msg::CameraState camState;
 
     unsigned int imageWidth;
     unsigned int imageHeight;
@@ -46,6 +95,7 @@ class GenPointCloudNode : public rclcpp::Node {
     float vFov;
 
     const unsigned int AXIS_COUNT;
+    const float SCALE_FACTOR;
 
     std::vector<float> u;
     std::vector<float> v;
